d1057bff798a25269a9c7886d805cedfe35c6dd0
VladBobarika <vlad.bobarika@gmail.com>
2021-12-14 21:39:01 +0300

    create brand model and factory brand

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
+use App\Models\Category;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +19,47 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $list = Product::get();
+    dd($list);
+    //$product = Product::find(3);
+    //    $date = [
+    //        'name' => 'Iphone 12'
+    //    ];
+    //    $product = Product::create($date);
+    //$product = new Product();
+    //$product->name = "Samsung x20";
+    //$product->save();
+    //dd($product);
+    //dd(Product::all());
+
+    return view('main');
 });
+
+Route::get('product/store', function () {
+    return view('store');
+});
+
+Route::get('/product', function () {
+    $category = new Category();
+    $category->name = 'Mobile phone';
+    $category->status = true;
+    $category->save();
+
+    $data = [
+        'name' => 'Xiaomi mi 11',
+        'status' => true
+    ];
+    $category = Category::create($data);
+    return view('product');
+});
+
+Route::get('catalog', [ProductController::class, 'catalog']) ->name('catalog');
+
+Route::get('hello', [SiteController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
